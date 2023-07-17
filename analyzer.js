475a1753308cb7ce660f601270b4d92abd63b93b
isamon96 <imondragon920@gmail.com>
2023-07-17 08:52:01 -0600

    Updates

---
+++ analyzer.js
@@ -0,0 +1,46 @@
+const analyzer = {  
+  getWordCount: (text) => {
+    text = text.replace(/[^\w\s]/g, '').trim();
+    let words = text.split(" ");
+    words = words.filter(word => word.length > 0);
+    return words.length;
+  },
+  
+  getCharacterCount: (text) => text.length,
+
+  getCharacterCountExcludingSpaces: (text) => {
+    const regex = /[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~¡¿«»“”‘’´¨\s]/g;
+    text = text.replace(regex,"");
+    return text.length;
+  },
+
+  getAverageWordLength: (text) => {
+    const regex = /[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~¡¿«»“”‘’´¨\d]/g;
+    text = text.replace(regex,"").trim();
+    const words = text.split(" ");
+    let numberCharacters = 0;
+    words.forEach(word => numberCharacters += word.length);
+    const average = numberCharacters/words.length;
+    return parseFloat(average.toFixed(2));
+  },
+
+  getNumberCount: (text) => {
+    const numbersAmount = text.match(/\b\d+(\.\d+)?\b/g);
+    return (numbersAmount !== null) ? numbersAmount.length : 0;
+  },
+
+  getNumberSum: (text) => {
+    const numbers = text.match(/\b\d+(\.\d+)?\b/g);
+    let sum = 0;
+    if (numbers !== null) {
+      numbers.forEach(number => {
+        sum += parseFloat(number);
+      });
+    }
+    return sum; 
+  },
+};
+
+export default analyzer;
+
+
---
+++ index.js
@@ -0,0 +1,39 @@
+import analyzer from './analyzer.js';
+
+const resetButton = document.getElementById('reset-button');
+const textarea = document.querySelector('textarea[name="user-input"]');
+const liWordCount = document.querySelector('li[data-testid="word-count"]')
+const liCharacterCount = document.querySelector('li[data-testid="character-count"]');
+const liCharacterNoSpaces = document.querySelector('li[data-testid="character-no-spaces-count"]');
+const liNumberCount = document.querySelector('li[data-testid="number-count"]');
+const liNumberSum = document.querySelector('li[data-testid="number-sum"]');
+const liWordAverage = document.querySelector('li[data-testid="word-length-average"]');
+
+
+resetButton.addEventListener('click', cleanTextArea);
+textarea.addEventListener('input', updateMetrics);
+
+function cleanTextArea () {
+  document.querySelector('textarea[name="user-input"]').value = "";
+  updateMetrics();
+}
+
+function updateMetrics() {
+  const text = textarea.value;
+  const wordCount = analyzer.getWordCount(text);
+  const characterCount = analyzer.getCharacterCount(text);
+  const characterNoSpaces = analyzer.getCharacterCountExcludingSpaces(text);
+  const numberCount = analyzer.getNumberCount(text);
+  const numberSum = analyzer.getNumberSum(text);
+  const wordAverage = analyzer.getAverageWordLength(text);
+
+  liWordCount.textContent = `PALABRAS: ${wordCount}`;
+  liCharacterCount.textContent = `CARACTERES: ${characterCount}`;
+  liCharacterNoSpaces.textContent = `CARACTERES SIN SIGNOS NI ESPACIOS: ${characterNoSpaces}`;
+  liNumberCount.textContent = `NÚMEROS: ${numberCount}`;
+  liNumberSum.textContent = `SUMA: ${numberSum}`;
+  liWordAverage.textContent = `LONGITUD MEDIA: ${wordAverage}`;
+
+}
+
+
